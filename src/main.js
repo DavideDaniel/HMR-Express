ee872a26d4fb12a9853af3cc806c640639686b5e
DavideDaniel <davidedaniel@gmail.com>
2016-04-18 19:47:06 -0700

    grouped bar chart lesson

--- src/main.js
+++ src/main.js
@@ -1,117 +1,167 @@
 import d3 from 'd3';
-import _ from 'lodash';
 import './assets/styles.css';
-import $ from 'jquery';
+import { queue } from 'd3-queue';
+// import * as d3_request from 'd3-request';
+window.queue = queue;
+// window.request = d3_request;
 
-// using lodash for random data
-const dataSet = _.map(_.range(25), () => {
-  return {
-    x: Math.round(Math.random() * 100),
-    y: Math.round(Math.random() * 100),
-    radius: Math.round(Math.random() * 40)
-  };
-});
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 30,
+  left: 40
+};
 
-const newDate = '03-21-16'
-let formattedDate = d3.time.format('%m-%d-%y').parse(newDate);
-console.log(formattedDate)
-$('#change').on('click', changeData);
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
 
-function changeData(){
-  dataSet.forEach(datum =>{
-    datum.x = Math.round(Math.random() * 100);
-    datum.y = Math.round(Math.random() * 100);
-    datum.radius = Math.round(5 + Math.random() * 40);
-  })
-  svg.selectAll('circle')
-    .transition()
-    .duration(1000)
-    .attr('cx', function(data) {
-      return xScale(data.x);
-    })
-    .transition()
-    .duration(1000)
-    .attr('cy', function(data) {
-      return yScale(data.y);
-    })
-    .style('fill', 'cornflowerBlue')
-    .transition()
-    .duration(1000)
-    .attr('r', function(data) {
-      return data.radius;
-    })
-    .style('fill', 'tomato');
-}
-
-// all d3 code below
-
-var margin = {top: 20, right: 20, bottom: 60, left: 40};
-
-var width = 600 - margin.left - margin.right;
-var height = 400 - margin.top - margin.bottom;
-
-var svg = d3.select('#content').append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-  .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')'); // transform the x,y value with translate
-
-var yScale = d3.scale.linear()
-  .domain([0, d3.max(dataSet, function(data) {
-    return data.y;
-  })])
+const x0 = d3.scale.ordinal()
+  .rangeRoundBands([0, width], .1);
+const x1 = d3.scale.ordinal();
+const y = d3.scale.linear()
   .range([height, 0]);
+const color = d3.scale.ordinal()
+  .range(['RGBA(215, 59, 65, .8)', 'RGBA(252, 179, 111, .80)', 'RGBA(77, 146, 194, .80)','RGBA(182, 223, 170, 1.00)']);
 
-var xScale = d3.scale.linear()
-  .domain([0, 100])
-  .range([0, width]);
+const xAxis = d3.svg.axis()
+  .scale(x0)
+  .orient("bottom");
+const yAxis = d3.svg.axis()
+  .scale(y)
+  .orient("left")
+  .tickFormat(d3.format(".2s"));
 
-var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient('bottom')
-    .ticks(10)
-    .innerTickSize(10)
-    .outerTickSize(10)
-    .tickPadding(10);
+const svg = d3.select("body")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//
+//
+  // using queue to collect multiple data sets
+  const collectData = new Promise((resolve, reject) => {
+       queue()
+         .defer(d3.json, 'http://54.213.83.132/hackoregon/http/committee_data_by_id/12498/')
+         .defer(d3.json, 'http://54.213.83.132/hackoregon/http/committee_data_by_id/5591/')
+         .defer(d3.json, 'http://54.213.83.132/hackoregon/http/committee_data_by_id/15089/')
+         .defer(d3.json, 'http://54.213.83.132/hackoregon/http/committee_data_by_id/12456/')
+         .await((err, a, b, c, d) => {
+           if(err){
+             reject(err);
+           }
+           let dataSet = [...a, ...b, ...c, ...d]; // spread operator
+           resolve(dataSet);
+         });
+     });
+//
+function visualize(data) {
+  // console.log(data)
+  // format
+  const dataSet = data.map((item) => {
+    return {
+      'Candidate': item.candidate_name,
+      'Total': item.total,
+      'Total spent': item.total_spent,
+      'In State': item.instate * item.total,
+      'Grassroots': item.grassroots * item.total,
+    }
+  });
 
-svg.append('g')
-  .attr('class', 'x axis')
-  .attr('transform', 'translate(0, '+ (height + 10) + ')') // moved to height + 10 pixels
-  .call(xAxis);
+const keyNames = d3.keys(dataSet[0])
+    .filter(key => key !== 'Candidate');
+  dataSet.forEach((d) => {
+    d.keys = keyNames.map((key) => {
+      return {
+        name: key,
+        value: +d[key]
+      }
+    });
+  });
+  // console.log(dataSet, keyNames);
+  x0.domain(dataSet.map(d => d.Candidate));
+  x1.domain(keyNames)
+    .rangeRoundBands([0, x0.rangeBand()]);
+  y.domain([0, d3.max(dataSet, d => d3.max(d.keys, d => d.value))])
 
-  var yAxis = d3.svg.axis()
-  .scale(yScale)
-  .orient('left')
-  .ticks(5)
-  .innerTickSize(10)
-  .outerTickSize(2)
-  .tickPadding(10);
-
-  svg.append('g')
-    .attr('class', 'y axis')
-    .attr('transform', 'translate(0, '+ 10 + ')') // moved to height + 10 pixels
-    .call(yAxis);
-
-svg.selectAll('circle')
-  .data(dataSet)
-  .enter()
-  .append('circle') // creating circles
-  .attr('class', 'bubble')
-  .attr('cx', function(data) { // determining the center point x value
-    return xScale(data.x);
+//
+//   // draw axes
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "translate(0" + 50 + ")")
+    .attr("y", 6)
+    .attr("dy", ".5em")
+    .style("text-anchor", "end")
+    .text("Dollars");
+//
+//   // draw candidate dataset
+  const candidate = svg.selectAll(".candidate")
+    .data(dataSet)
+    .enter()
+    .append("g")
+    .attr("class", "candidate")
+    .attr("transform", (d) => {
+      return "translate(" + x0(d.Candidate) + ",0)";
+    });
+  candidate.selectAll("rect")
+    .data((d) => {
+      return d.keys;
+    })
+    .enter()
+    .append("rect")
+    .attr("width", x1.rangeBand())
+    .attr("x", (d) => {
+      return x1(d.name);
+    })
+    .attr("y", (d) => {
+      return y(d.value);
+    })
+    .attr("height", (d) => {
+      return height - y(d.value);
+    })
+    .style("fill", (d) => {
+      return color(d.name);
+    });
+//
+//   // draw legend
+  const legend = svg.selectAll(".legend")
+    .data(keyNames.slice())
+    .enter()
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => {
+      return "translate(0," + i * 20 + ")";
+    });
+  legend.append("rect")
+    .attr("x", width - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", color);
+  legend.append("text")
+    .attr("x", width - 24)
+    .attr("y", 6)
+    .attr("dy", ".5em")
+    .style("text-anchor", "end")
+    .text((d) => {
+      return d;
+    });
+}
+//
+collectData // promise - not a function
+  .then(value => {
+    // console.log(value)
+    return visualize(value);
   })
-  .attr('cy', function(data) { // determining the center point y value
-    return yScale(data.y);
-  })
-  .attr('r', function(data) { // determining the radius of the circle
-    return data.radius;
-  })
-  .on('mouseover', function(d){
-    d3.select(this).classed('highlight', true);
-  })
-  .on('mouseout', function(d){
-    d3.select(this).classed('highlight',false);
-  })
+//   .then(value => {
+//     // what else can we do?
+//   });
+
 
 /*
  * ignore this code below - it's for webpack to know that this
