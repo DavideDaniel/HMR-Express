4d089c5d06c2fef62149bb2aa57b367a15cabee7
DavideDaniel <davidedaniel@gmail.com>
2016-07-20 19:15:04 -0700

    work

--- src/main.js
+++ src/main.js
@@ -4,167 +4,100 @@ import { queue } from 'd3-queue';
 // import * as d3_request from 'd3-request';
 window.queue = queue;
 // window.request = d3_request;
+// const collectData = new Promise((resolve, reject) => {
+//      queue()
+//        .defer(d3.csv, './hackORVolunteers.csv')
+//        .await((err, a) => {
+//          if(err){
+//            reject(err);
+//          }
+//          let dataSet = [...a]; // spread operator
+//          resolve(dataSet);
+//        });
+//    });
 
-const margin = {
-  top: 20,
-  right: 20,
-  bottom: 30,
-  left: 40
-};
+// collectData.then(value => {
+//   console.log(value);
+// })
 
-const width = 960 - margin.left - margin.right;
-const height = 500 - margin.top - margin.bottom;
+d3.csv('./hackORVolunteers.csv', function(d){
+  console.log(d)
+   return {
+     response: d["What motivated you most to volunteer with Hack Oregon?"].split(',')
+     }
+   }, function(err,rows){
+   var hashSet = {}
+   var num = 0;
 
-const x0 = d3.scale.ordinal()
-  .rangeRoundBands([0, width], .1);
-const x1 = d3.scale.ordinal();
-const y = d3.scale.linear()
-  .range([height, 0]);
-const color = d3.scale.ordinal()
-  .range(['RGBA(215, 59, 65, .8)', 'RGBA(252, 179, 111, .80)', 'RGBA(77, 146, 194, .80)','RGBA(182, 223, 170, 1.00)']);
+   for (var i = 0; i < rows.length; i++) {
+     var response = rows[i].response;
+     for (var j = 0; j < response.length; j++) {
+       var answer = response[j].trim();
+       if(!Object.keys(hashSet).includes(answer)){
+         hashSet[answer] = 1;
+       } else {
+         hashSet[answer] = hashSet[answer]+1;
+       }
+     }
+   }
+   console.log(hashSet)
+});
+  //  var dataSet = Object.keys(hashSet).map(function(item){
+  //    if (key);
+  //  })
 
-const xAxis = d3.svg.axis()
-  .scale(x0)
-  .orient("bottom");
-const yAxis = d3.svg.axis()
-  .scale(y)
-  .orient("left")
-  .tickFormat(d3.format(".2s"));
 
-const svg = d3.select("body")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  
-  // using queue to collect multiple data sets
-  const collectData = new Promise((resolve, reject) => {
-       queue()
-         .defer(d3.json, 'http://54.213.83.132/hackoregon/http/committee_data_by_id/12498/')
-         .defer(d3.json, 'http://54.213.83.132/hackoregon/http/committee_data_by_id/5591/')
-         .defer(d3.json, 'http://54.213.83.132/hackoregon/http/committee_data_by_id/15089/')
-         .defer(d3.json, 'http://54.213.83.132/hackoregon/http/committee_data_by_id/12456/')
-         .await((err, a, b, c, d) => {
-           if(err){
-             reject(err);
-           }
-           let dataSet = [...a, ...b, ...c, ...d]; // spread operator
-           resolve(dataSet);
-         });
-     });
+
+  // var data = d3.csv.parse('hackORVolunteers.csv');
+
+// //get variable names
+//   var keys = d3.keys(data[0]);
+//               var namesTitle = keys[0];
+//               var valuesTitle = keys[1];
 //
-function visualize(data) {
-  // console.log(data)
-  // format
-  const dataSet = data.map((item) => {
-    return {
-      'Candidate': item.candidate_name,
-      'Total': item.total,
-      'Total spent': item.total_spent,
-      'In State': item.instate * item.total,
-      'Grassroots': item.grassroots * item.total,
-    }
-  });
-
-const keyNames = d3.keys(dataSet[0])
-    .filter(key => key !== 'Candidate');
-  dataSet.forEach((d) => {
-    d.keys = keyNames.map((key) => {
-      return {
-        name: key,
-        value: +d[key]
-      }
-    });
-  });
-  // console.log(dataSet, keyNames);
-  x0.domain(dataSet.map(d => d.Candidate));
-  x1.domain(keyNames)
-    .rangeRoundBands([0, x0.rangeBand()]);
-  y.domain([0, d3.max(dataSet, d => d3.max(d.keys, d => d.value))])
-
+//               //accessing the properties of each object with the variable name through its key
+//               var values = function(d) {return +d[valuesTitle];};
+//               var names = function(d) {return d[namesTitle];}
+// console.log(dirtyCSV);
+// var height = 250; // set vars for height & width
+// var width = 600;
 //
-//   // draw axes
-  svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-  svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "translate(0" + 50 + ")")
-    .attr("y", 6)
-    .attr("dy", ".5em")
-    .style("text-anchor", "end")
-    .text("Dollars");
+// var yScale = d3.scale.linear()
+//   .domain([0, d3.max(dataSet)*1.1]) // domain now with d3.max
+//   .range([0, height]) // set yScale linear
+// var xScale = d3.scale.ordinal() // orders
+//   .domain(dataSet)
+//   .rangeBands([0, width], 0.25, 0.25); // (width of data), padding between, padding outside
 //
-//   // draw candidate dataset
-  const candidate = svg.selectAll(".candidate")
-    .data(dataSet)
-    .enter()
-    .append("g")
-    .attr("class", "candidate")
-    .attr("transform", (d) => {
-      return "translate(" + x0(d.Candidate) + ",0)";
-    });
-  candidate.selectAll("rect")
-    .data((d) => {
-      return d.keys;
-    })
-    .enter()
-    .append("rect")
-    .attr("width", x1.rangeBand())
-    .attr("x", (d) => {
-      return x1(d.name);
-    })
-    .attr("y", (d) => {
-      return y(d.value);
-    })
-    .attr("height", (d) => {
-      return height - y(d.value);
-    })
-    .style("fill", (d) => {
-      return color(d.name);
-    })
-    .append("text")
-    .style("text-anchor","end")
-    .text(d => d.value)
-    .attr('y' )
-
+//   var colorScale = d3.scale.linear() // linear - min / max
+//   //.domain([0,d3.max(dataSet)]) // for data based
+//   .domain([0,dataSet.length]) // for position based
+//   .range(['tomato','cornflowerBlue'])
 //
-//   // draw legend
-  const legend = svg.selectAll(".legend")
-    .data(keyNames.slice())
-    .enter()
-    .append("g")
-    .attr("class", "legend")
-    .attr("transform", (d, i) => {
-      return "translate(0," + i * 20 + ")";
-    });
-  legend.append("rect")
-    .attr("x", width - 18)
-    .attr("width", 18)
-    .attr("height", 18)
-    .style("fill", color);
-  legend.append("text")
-    .attr("x", width - 24)
-    .attr("y", 6)
-    .attr("dy", ".5em")
-    .style("text-anchor", "end")
-    .text((d) => {
-      return d;
-    });
-}
+// var svg = d3.select('#barChart').append('svg')
+//   .attr('width', width)
+//   .attr('height', height)
 //
-collectData // promise - not a function
-  .then(value => {
-    return visualize(value);
-  }).then(value => {
-//     // what else can we do?
-  });
-
-
+// svg.selectAll('rect')
+//   .data(dataSet)
+//   .enter()
+//   .append('rect')
+//   .attr('class', 'bar')
+//   .attr('x', function(data, index) {
+//     return xScale(data) // using xScale on data
+//   })
+//   .attr('y', function(data) {
+//     return height - yScale(data) // using yScale on data
+//   })
+//   .attr('width', xScale.rangeBand) // width determined by xScale.rangeBand
+//   .style('height', function(data) {
+//     return yScale(data) // height determined by yScale
+//   })
+//   .attr('fill', function(data,i) {
+//    // return colorScale(data) // for data based
+//     return colorScale(i) // for position based
+//   })
+// });
 /*
  * ignore this code below - it's for webpack to know that this
  * code needs to be watched and not to append extra elements
