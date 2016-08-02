8020d82dd483b655bac281a31a38219367ee4440
DavideDaniel <davidedaniel@gmail.com>
2016-08-01 20:22:34 -0700

    week3 -1

--- src/main.js
+++ src/main.js
@@ -1,17 +1,33 @@
 import d3 from 'd3';
 import './assets/normalize.css';
 import './assets/styles.css';
-
 const colorBlend = d3.interpolateRgb('#CDF3F2', '#08519c');
 
 function donorPercent(amount,donors) {
+  // at 0 it should be size: 0%, color: #fff
+  if(!amount) {
+    return  {
+      size: '0%',
+      color: '#fff'
+    }
+  }
+
   //  value of the highest donor
+  let donorMax = d3.max(donors, d => d.value);
+  console.log(donorMax);
+  let donorSize = d3.scale.linear().domain([0,donorMax]).range([0,1]);
+  console.log(donorSize);
+
+  return {
+    size: `${100 * donorSize(amount)}%`,
+    color: colorBlend(donorSize(amount))
+  }
 
   // % for size
-  size:
-
+  // size: '100%'
   // color
-  color:
+  // color: '#08519c'
+
 }
 
 // dummy data
@@ -21,5 +37,10 @@ const donors = [
   {"name":"Michael Bloomberg","value":2385000},
   {"name":"Loren Parks","value":896800},
   {"name":"Connie Ballmer","value":505000},
-  {"name":"Tom Hormel","value":500000}
+  {"name":"Tom Hormel","value":500000},
+
 ];
+// {"name":"John K", "value":25}
+console.log(donorPercent(250000, donors));
+
+
