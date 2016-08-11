b93e222cc34fe9258887642dc50ec719db73b39a
DavideDaniel <davidedaniel@gmail.com>
2016-08-10 19:02:21 -0700

    adjustments

--- src/main.js
+++ src/main.js
@@ -2,146 +2,70 @@ import d3 from 'd3';
 import './assets/normalize.css';
 import './assets/styles.css';
 import data from './example';
-
-const margin = {top: 20, right: 120, bottom: 20, left: 120},
-    width = 960 - margin.right - margin.left,
-    height = 800 - margin.top - margin.bottom;
-
-let i = 0,
-    duration = 750,
-    root;
-
-const tree = d3.layout.tree()
-    .size([height, width]);
-
-const diagonal = d3.svg.diagonal()
-    .projection(function(d) { return [d.y, d.x]; });
-
-const svg = d3.select("#content").append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
+const url = 'https://gist.githubusercontent.com/d3byex/25129228aa50c30ef01f/raw/2f8664d0eb75ef606f412f9647116954ff0af41d/radial.json'; //url='https://gist.githubusercontent.com/d3byex/25129228aa50c30ef01f/raw/ddc1d7cf560f8fff84dd9005e5bdbdd43b39a91f/flare.json';
+d3.json(url, function (error, data) {
+  const width = 600,
+    height = 600,
+    nodeRadius = 4.5;
+  const svg = d3.select('#content')
+    .append('svg')
+    .attr({
+      width: width,
+      height: height
+    });
+  const radius = width / 2;
+  const mainGroup = svg.append('g')
+    .attr("transform", "translate(" + radius + "," + radius + ")");
+  const cluster = d3.layout.cluster()
+    .size([360, radius - 50]);
+  const nodes = cluster.nodes(data);
+  const links = cluster.links(nodes);
+  const diagonal = d3.svg.diagonal.radial()
+    .projection(d => {
+      return [
+        d.y,
+        d.x / 180 * Math.PI
+      ];
+    });
+  mainGroup.selectAll('path')
+    .data(links)
+    .enter()
+    .append('path')
+    .attr({
+      'd': diagonal,
+      fill: 'none',
+      stroke: '#ccc',
+      'stroke-width': 1
+    });
+  const nodeGroups = mainGroup.selectAll("g")
+    .data(nodes)
+    .enter()
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-  // if (error) throw error;
-
-  root = data;
-  root.x0 = height / 2;
-  root.y0 = 0;
-
-  function collapse(d) {
-    if (d.children) {
-      d._children = d.children;
-      d._children.forEach(collapse);
-      d.children = null;
-    }
-  }
-
-  root.children.forEach(collapse);
-  update(root);
-
-
-d3.select(self.frameElement).style("height", "800px");
-
-function update(source) {
-
-  // Compute the new tree layout.
-  const nodes = tree.nodes(root).reverse(),
-      links = tree.links(nodes);
-
-  // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 180; });
-
-  // Update the nodes…
-  const node = svg.selectAll("g.node")
-      .data(nodes, function(d) { return d.id || (d.id = ++i); });
-
-  // Enter any new nodes at the parent's previous position.
-  const nodeEnter = node.enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-      .on("click", click);
-
-  nodeEnter.append("circle")
-      .attr("r", 1e-6)
-      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-  nodeEnter.append("text")
-      .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
-      .attr("dy", ".35em")
-      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-      .text(function(d) { return d.name; })
-      .style("fill-opacity", 1e-6);
-
-  // Transition nodes to their new position.
-  const nodeUpdate = node.transition()
-      .duration(duration)
-      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-
-  nodeUpdate.select("circle")
-      .attr("r", 4.5)
-      .style("fill", function(d) { return d._children ? "tomato" : "#fff"; });
-
-  nodeUpdate.select("text")
-      .style("fill-opacity", 1);
-
-  // Transition exiting nodes to the parent's new position.
-  const nodeExit = node.exit().transition()
-      .duration(duration)
-      .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
-      .remove();
-
-  nodeExit.select("circle")
-      .attr("r", 1e-6);
-
-  nodeExit.select("text")
-      .style("fill-opacity", 1e-6);
-
-  // Update the links…
-  const link = svg.selectAll("path.link")
-      .data(links, function(d) { return d.target.id; });
-
-  // Enter any new links at the parent's previous position.
-  link.enter().insert("path", "g")
-      .attr("class", "link")
-      .attr("d", function(d) {
-        var o = {x: source.x0, y: source.y0};
-        return diagonal({source: o, target: o});
-      });
-
-  // Transition links to their new position.
-  link.transition()
-      .duration(duration)
-      .attr("d", diagonal);
-
-  // Transition exiting nodes to the parent's new position.
-  link.exit().transition()
-      .duration(duration)
-      .attr("d", function(d) {
-        var o = {x: source.x, y: source.y};
-        return diagonal({source: o, target: o});
-      })
-      .remove();
-
-  // Stash the old positions for transition.
-  nodes.forEach(function(d) {
-    d.x0 = d.x;
-    d.y0 = d.y;
-  });
-}
-
-// Toggle children on click.
-function click(d) {
-  if (d.children) {
-    d._children = d.children;
-    d.children = null;
-  } else {
-    d.children = d._children;
-    d._children = null;
-  }
-  update(d);
-}
+    .attr("transform", d => {
+      return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
+    });
+  nodeGroups.append("circle")
+    .attr({
+      r: nodeRadius,
+      fill: '#fff',
+      stroke: 'tomato',
+      'stroke-width': 1.5
+    });
+  nodeGroups.append("text")
+    .attr({
+      dy: ".31em",
+      'text-anchor': d => {
+        return d.x < 180 ? "start" : "end";
+      },
+      'transform': d => {
+        return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)";
+      }
+    })
+    .style('font', '12px Open Sans')
+    .text(d => {
+      return d.name;
+    });
+});
 /*
  * ignore this code below - it's for webpack to know that this
  * code needs to be watched and not to append extra elements
